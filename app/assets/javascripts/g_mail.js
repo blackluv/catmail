d8bad8ada188b1a7bdeed4e7a50e757f97e9a61f
Dylan Patrick Clark <dylanpatrickclark@gmail.com>
2013-11-18 11:20:23 -0800

    Generate backbone.js scaffold.

---
+++ app/assets/javascripts/g_mail.js
@@ -0,0 +1,13 @@
+window.GMail = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  GMail.initialize();
+});
