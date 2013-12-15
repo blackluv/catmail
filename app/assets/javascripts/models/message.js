623e3df15bdb1ac80eed27cef511ca4dc2df8abb
Dylan Patrick Clark <dylanpatrickclark@gmail.com>
2013-12-15 14:26:54 -0800

    Get rid of message#parse.

--- app/assets/javascripts/models/message.js
+++ app/assets/javascripts/models/message.js
@@ -1,12 +1,3 @@
 GM.Models.Message = Backbone.Model.extend({
     url: '/api/v1/messages',
-
-    parse: function(response) {
-        if (response.message) {
-            delete(response.message.id);
-            _.extend(response, response.message);
-            delete(response.message);
-        }
-        return response;
-    },
 });
