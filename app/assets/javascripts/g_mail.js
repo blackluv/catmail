8f2ab9a6ef27ee5ac3668047488d5be62b84025e
Dylan Patrick Clark <dylanpatrickclark@gmail.com>
2013-12-17 14:59:32 -0800

    Loading spinner.

--- app/assets/javascripts/g_mail.js
+++ app/assets/javascripts/g_mail.js
@@ -8,7 +8,16 @@ window.GM = {
 
     initialize: function() {
         // They're going to want the inbox.
-        GM.Store.inbox.fetch();
+        $(".loading-area__bar__fill").addClass('filling');
+
+        GM.Store.inbox.fetch({
+            success: function() {
+                // Fake slower load time to show off load bar... Sorry.
+                window.setTimeout(function () {
+                    $(".loading-area__bar__fill").removeClass('filling').addClass('done');
+                    $('#load').fadeOut(300);
+                }, 1000);
+        }});
         new GM.Routers.Mail({
             rootSel: '#content',
             sidebarSel: '#sidebar'
